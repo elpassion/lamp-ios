14c2142a3ae798addd3c8414c2646e07e2803a90
Jakub Turek <jkbturek@gmail.com>
2019-01-10 10:34:09 +0100

    Add empty Xcode project

---
+++ LAMP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LAMP
+//
+//  Created by Jakub Turek on 10/01/2019.
+//  Copyright © 2019 EL Passion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
